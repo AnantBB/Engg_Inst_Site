724f6ab92317669358d039708ddbf61e2b9dc778
AnantBB <72018443+AnantBB@users.noreply.github.com>
2023-10-10 14:38:37 +0530

    Update settings.py

--- Engg_Inst_Site/settings.py
+++ Engg_Inst_Site/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'Engg_Inst_Site.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -91,7 +91,7 @@ DATABASES = {
         'PORT': '5432'   
       }
     }
-"""
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
